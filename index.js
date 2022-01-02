a0726f529226f1f2382f902fd51589d1dfcf23ec
Rahim BAH <abdoulrahim.bahh@gmail.com>
2022-01-02 02:07:17 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const textAnim = document.querySelector('h2');
+
+new Typewriter(textAnim, {
+
+    
+})
+.changeDelay(20)
+.Typewriter("Hello moi cest Abdoul Rahim, développeur en React,")
+.pauseFor(300)
+.deleteChars(12)
+Typewriter('<strong>Ingenieur Biologiste / Informaticien</strong>')
+.start();
